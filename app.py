e6775224ae07b2e9bd3df4290580ac5795b2ce4e
0xsmrpn <samarpan_d@pp.iitr.ac.in>
2024-09-05 09:20:54 +0530

    feat: add async runners in app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 from slippage_estimator import get_output_token, get_add_lp, get_remove_single_lp, get_remove_lp
+import asyncio
 
 app = Flask(__name__)
 CORS(app)
@@ -8,12 +9,12 @@ CORS(app)
 
 @cross_origin(origins=["http://localhost:3000", "https://app.lucidly.finance"])
 @app.route('/get_output_token', methods=['GET'])
-async def get_output_token_route():
+def get_output_token_route():
     _i = int(request.args.get('_i'))
     _j = int(request.args.get('_j'))
     _dx = int(request.args.get('_dx'))
     try:
-        result = await get_output_token(_i, _j, _dx)
+        result = asyncio.run(get_output_token(_i, _j, _dx))
         return jsonify({'result': int(result)})
     except ValueError as e:
         return jsonify({"error": str(e)}), 400
@@ -23,11 +24,11 @@ async def get_output_token_route():
 
 @cross_origin(origins=["http://localhost:3000", "https://app.lucidly.finance"])
 @app.route('/get_add_lp', methods=['POST'])
-async def get_add_lp_route():
+def get_add_lp_route():
     _amounts = request.json.get('_amounts')
 
     try:
-        result = await get_add_lp(_amounts)
+        result = asyncio.run(get_add_lp(_amounts))
         return jsonify({'result': int(result)})
     except ValueError as e:
         return jsonify({"error": str(e)}), 400
@@ -37,10 +38,10 @@ async def get_add_lp_route():
 
 @cross_origin(origins=["http://localhost:3000", "https://app.lucidly.finance"])
 @app.route('/get_remove_lp', methods=['GET'])
-async def get_remove_lp_route():
+def get_remove_lp_route():
     _mvt_amount = int(request.args.get('_mvt_amount'))
     try:
-        result = await get_remove_lp(_mvt_amount)
+        result = asyncio.run(get_remove_lp(_mvt_amount))
         return jsonify({'result': result})
     except ValueError as e:
         return jsonify({"error": str(e)}), 400
@@ -50,11 +51,11 @@ async def get_remove_lp_route():
 
 @cross_origin(origins=["http://localhost:3000", "https://app.lucidly.finance"])
 @app.route('/get_remove_single_lp', methods=['GET'])
-async def get_remove_single_lp_route():
+def get_remove_single_lp_route():
     _token = int(request.args.get('_token'))
     _mvt_amount = int(request.args.get('_mvt_amount'))
     try:
-        result = await get_remove_single_lp(_token, _mvt_amount)
+        result = asyncio.run(get_remove_single_lp(_token, _mvt_amount))
         return jsonify({'result': int(result)})
     except ValueError as e:
         return jsonify({"error": str(e)}), 400
@@ -62,5 +63,5 @@ async def get_remove_single_lp_route():
         return jsonify({"error": f"Missing parameter: {str(e)}"}), 400
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#  if __name__ == '__main__':
+#      app.run(debug=True)
